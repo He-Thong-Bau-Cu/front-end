033bfedbc52043b61c110db3dd622207131a6492
GiangHoangg <147378405+GiangHoangg@users.noreply.github.com>
2025-11-10 04:45:57 +0700

    call api dashboard

--- src/services/VoterService.tsx
+++ src/services/VoterService.tsx
@@ -1,9 +1,19 @@
+import { ApiResponse } from "@/types/ApiResponse.interface";
 import BaseService from "./BaseService";
+import { DashboardVoterStats } from "@/types/Voter.interface";
 
 class VoterService extends BaseService {
     constructor() {
         super("voters");
     }
+
+
+
+    async getDashboardVoterByElectionId(id: string | number): Promise<DashboardVoterStats> {
+        const response = await this.api.get(`${this.endpoint}/dashboard/${id}`) as ApiResponse<DashboardVoterStats>;
+        return response.data;
+    }
+
 }
 
 export default new VoterService();
