c3d6a427e61e4a4f4e3e2583bc6abc0233288daf
anlp-dev <anlp.sep19@gmail.com>
2025-12-04 17:08:00 +0700

    update code

--- src/utils/format.tsx
+++ src/utils/format.tsx
@@ -1,16 +1,22 @@
 import moment from 'moment-timezone';
 
+const TARGET_OFFSET_MINUTES = -7 * 60;
+
+const withTargetOffset = (date: Date) => {
+  return moment(date).utcOffset(TARGET_OFFSET_MINUTES);
+};
+
 export const getTimeAgo = (date: Date) => {
-  return moment(date).fromNow();
-}
+  return withTargetOffset(date).fromNow();
+};
 
 export const formatDate = (date: Date) => {
-  return moment(date).format('DD-MM-YYYY HH:mm:ss');
-}
+  return withTargetOffset(date).format('DD-MM-YYYY HH:mm:ss');
+};
 
 export const formatDateOfBirth = (date: Date) => {
-  return moment(date).format('DD-HH-YYYY');
-}
+  return withTargetOffset(date).format('DD-MM-YYYY');
+};
 
 export const formatSecondsToClock = (seconds?: number | null) => {
   if (seconds === undefined || seconds === null || Number.isNaN(seconds)) {
